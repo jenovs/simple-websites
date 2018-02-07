509ff6b5aa78c1bc12e6ef892d55334a2051815f
Viktors Jenovs <jviktors@gmail.com>
2018-02-07 19:42:48 +0200

    Add code to handle 2nd level menu

--- grid-layout/main.js
+++ grid-layout/main.js
@@ -1,8 +1,29 @@
 const sideNavbar = document.querySelector('.side-navbar');
 const container = document.querySelector('.container');
 const overlay = document.querySelector('.overlay');
+const headerNavItems = document.querySelectorAll('.header__nav-item');
+const headerSubMenus = document.querySelectorAll('.header__sub-nav');
 
-document.querySelector('header').addEventListener('click', handleSidebar);
+headerNavItems.forEach(item => {
+  item.addEventListener('mouseenter', e => {
+    const id = e.target.dataset.submenu;
+    headerSubMenus.forEach((sub, i) => {
+      if (sub.dataset.submenu == id) {
+        headerSubMenus[i].classList.add('show');
+      } else if (id === undefined) {
+        headerSubMenus[i].classList.remove('show');
+      }
+    });
+  });
+
+  item.addEventListener('mouseleave', e => {
+    if (e.target.dataset.submenu) {
+      headerSubMenus[e.target.dataset.submenu].classList.remove('show');
+    }
+  });
+});
+
+document.querySelector('.hamburger').addEventListener('click', handleSidebar);
 
 overlay.onclick = () => {
   handleSidebar();
